cace24e2281734cddf9bed641b15fbc85289567d
Dev_nicolasj <104701931+devnicolasj@users.noreply.github.com>
2023-04-26 14:20:47 -0500

    Carpeta equivocada

--- java/ejercicio3.py
+++
@@ -1,4 +0,0 @@
-varPeso= int(input("¿Cuál es su peso en kg?:"))
-varEstatura = int(input('¿Cuál es su estatura en metros?: '))
-varResultado = varPeso / varEstatura
-print('Tu índice de masa corporal es: '+ str(round(varResultado,2)))
