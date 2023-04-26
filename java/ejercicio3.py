629aca03690d3ad470a24d6671c7ee8bbdda22f0
Dev_nicolasj <104701931+devnicolasj@users.noreply.github.com>
2023-04-26 14:20:16 -0500

    Añado ejercicio3 bootcamp de python

---
+++ java/ejercicio3.py
@@ -0,0 +1,4 @@
+varPeso= int(input("¿Cuál es su peso en kg?:"))
+varEstatura = int(input('¿Cuál es su estatura en metros?: '))
+varResultado = varPeso / varEstatura
+print('Tu índice de masa corporal es: '+ str(round(varResultado,2)))
